c309179a97a0d74027d8cb1e655efbbae4f2aa9a
code-master100 <keerthanadk777@gmail.com>
2025-11-30 19:34:11 +0530

    Prepare for Railway: add Procfile, runtime and requirements

--- AdoptaPaw/settings.py
+++ AdoptaPaw/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$3ra33ij5))$0hr$ncxhp73wfn!wl50cw4gpv9=%@z+pmb-vfc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
